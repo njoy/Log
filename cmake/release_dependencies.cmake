ce60d111c9a76dbe1fe7c4ada89ffbfc81b83012
Nathan Gibson <ngibson@lanl.gov>
2020-10-29 09:24:56 -0600

    Added release dependencies.

--- cmake/release_dependencies.cmake
+++ cmake/release_dependencies.cmake
@@ -5,15 +5,14 @@ include( FetchContent )
 # Declare project dependencies
 #######################################################################
 
-FetchContent_Declare( catch-adapter
-    GIT_REPOSITORY  https://github.com/njoy/catch-adapter
-    GIT_TAG         origin/master
-    GIT_SHALLOW     TRUE
-    )
-
 FetchContent_Declare( spdlog
     GIT_REPOSITORY  https://github.com/gabime/spdlog
     GIT_TAG         a51b4856377a71f81b6d74b9af459305c4c644f8
+    )
+
+FetchContent_Declare( catch-adapter
+    GIT_REPOSITORY  https://github.com/njoy/catch-adapter
+    GIT_TAG         fb84b82ebf7a4789aa43cea560680cf745c6ee4f
     )
 
 #######################################################################
@@ -21,6 +20,6 @@ FetchContent_Declare( spdlog
 #######################################################################
 
 FetchContent_MakeAvailable(
-    catch-adapter
     spdlog
+    catch-adapter
     )
