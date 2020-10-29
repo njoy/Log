dfa108720e3ecb9a344af7d9a2d28866918c7a22
Nathan Gibson <ngibson@lanl.gov>
2020-10-29 08:32:42 -0600

    Updated to new build system, including dependencies.

---
+++ cmake/develop_dependencies.cmake
@@ -0,0 +1,27 @@
+cmake_minimum_required( VERSION 3.14 )
+include( FetchContent )
+
+#######################################################################
+# Declare project dependencies
+#######################################################################
+
+FetchContent_Declare( catch-adapter
+    GIT_REPOSITORY  https://github.com/njoy/catch-adapter
+    GIT_TAG         origin/master
+    GIT_SHALLOW     TRUE
+    )
+
+FetchContent_Declare( spdlog
+    GIT_REPOSITORY  https://github.com/gabime/spdlog
+    GIT_TAG         a51b4856377a71f81b6d74b9af459305c4c644f8
+    )
+set( SPDLOG_BUILD_TESTING CACHE BOOL OFF )
+
+#######################################################################
+# Load dependencies
+#######################################################################
+
+FetchContent_MakeAvailable(
+    catch-adapter
+    spdlog
+    )
