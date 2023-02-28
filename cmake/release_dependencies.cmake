d4924b8be4fda930e270aeebe054ca172a456e4e
Tim Burke <tpburke@lanl.gov>
2023-02-28 16:59:05 -0700

    Updates for testing and installation CMake logic

--- cmake/release_dependencies.cmake
+++ cmake/release_dependencies.cmake
@@ -9,6 +9,7 @@ FetchContent_Declare( spdlog
     GIT_REPOSITORY  https://github.com/gabime/spdlog
     GIT_TAG         a51b4856377a71f81b6d74b9af459305c4c644f8
     )
+set( SPDLOG_BUILD_TESTING CACHE BOOL OFF )
 
 FetchContent_Declare( catch-adapter
     GIT_REPOSITORY  https://github.com/njoy/catch-adapter
@@ -21,5 +22,8 @@ FetchContent_Declare( catch-adapter
 
 FetchContent_MakeAvailable(
     spdlog
-    catch-adapter
     )
+
+if (${Log_unit_tests})
+    FetchContent_MakeAvailable(catch-adapter)
+endif()
