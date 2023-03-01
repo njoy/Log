55bfeaa7ad66e6952a0626fd1cacb5a6637ff434
Tim Burke <tpburke@lanl.gov>
2023-02-28 17:34:26 -0700

    Adds shacl_FetchContent in place of FetchContent

--- cmake/develop_dependencies.cmake
+++ cmake/develop_dependencies.cmake
@@ -1,18 +1,19 @@
-cmake_minimum_required( VERSION 3.14 )
-include( FetchContent )
+cmake_minimum_required( VERSION 3.24 )
+list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/.cmake)
+include( shacl_FetchContent )
 
 #######################################################################
 # Declare project dependencies
 #######################################################################
 
-FetchContent_Declare( catch-adapter
-    GIT_REPOSITORY  https://github.com/njoy/catch-adapter
+shacl_FetchContent_Declare( catch-adapter
+    GIT_REPOSITORY  ../../njoy/catch-adapter
     GIT_TAG         origin/master
     GIT_SHALLOW     TRUE
     )
 
-FetchContent_Declare( spdlog
-    GIT_REPOSITORY  https://github.com/gabime/spdlog
+shacl_FetchContent_Declare( spdlog
+    GIT_REPOSITORY  ../../gabime/spdlog
     GIT_TAG         a51b4856377a71f81b6d74b9af459305c4c644f8
     )
 set( SPDLOG_BUILD_TESTING CACHE BOOL OFF )
@@ -21,11 +22,11 @@ set( SPDLOG_BUILD_TESTING CACHE BOOL OFF )
 # Load dependencies
 #######################################################################
 
-FetchContent_MakeAvailable(
+shacl_FetchContent_MakeAvailable(
     spdlog
     )
 
 if (${Log_unit_tests})
-    FetchContent_MakeAvailable(catch-adapter)
+    shacl_FetchContent_MakeAvailable(catch-adapter)
 endif()
 
